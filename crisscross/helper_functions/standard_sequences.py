5c9eada09f1732b0c2614c7133b4eca648918379
Matthew <matthewaq31@gmail.com>
2024-02-07 23:45:59 -0500

    Added dwarves to list of standard sequences

--- crisscross/helper_functions/standard_sequences.py
+++ crisscross/helper_functions/standard_sequences.py
@@ -15,4 +15,20 @@ simpsons = {
     'Wiggum': 'GAATGTGTAAGGAGAATTTGG'
 }
 
+in_dwarves = {
+    'Fili': 'GAGACGATGTTGACCTTAACC',
+    'Kili': 'GGCTGAAAATCTCCTGACATG',
+    'Dwalin': 'GCTACTCACTCAGATAGGGTA',
+    'Balin': 'CCATCTTGTCATAACGGGAAG',
+    'Oin': 'ACTATATCGGTCGGAAACTGC',
+    'Gloin': 'AACGAGGACTCTTGGACTCTA',
+    'Ori': 'GTTTCTCCAAAAGCACTAGGG',
+    'Dori': 'CGAATTAGGAATACCGTGTCC',
+    'Nori': 'CGATCCTGATGTACGAAAGTC',
+    'Bifur': 'CCTGGAACACTTGCTAATGAG',
+    'Bofur': 'CGTGTAGCCAATTAGACTGAC',
+    'Bombur': 'TCTAACTTACAGAGCATGGCG'
+}
+
 simpsons_anti = {k: revcom(v) for k, v in simpsons.items()}
+in_dwarves_anti = {k: revcom(v) for k, v in in_dwarves.items()}
