dd7f17795694830dde28920c95b6987e62e900a5
Corey Becker <cmbecker1000@gmail.com>
2024-06-14 10:07:32 -0400

    Added drag & drop capabilities
    Does:
    1. Snap
    2. Move smoothly
    3. Move only element selected
    
    Does not:
    1. Check for overlap
    2. Allow multiple items to be moved at once

--- GUI/static/scripts/svgscript.js
+++ GUI/static/scripts/svgscript.js
@@ -1,4 +1,10 @@
 
+
+///////////////////////////////
+//     Helper Functions!     //
+///////////////////////////////
+
+//Draw grid:
 function drawGrid(gridGroup, width, height, style, majorSize, minorSize) {
     //Grid style: 
         //0 corresponds to no grid
@@ -50,10 +56,7 @@ function drawGrid(gridGroup, width, height, style, majorSize, minorSize) {
   }
 
 
-
-
-
-// Function to check if a point is on any existing line
+// Check if a point is on any existing line
 function isPointOnLine(drawing, x, y) {
     const lines = drawing.find('.line');
     return lines.some(line => {
@@ -64,6 +67,66 @@ function isPointOnLine(drawing, x, y) {
     });
   }
 
+
+
+///////////////////////////////
+//       Drag and Drop       //
+///////////////////////////////
+
+let selectedElement = null;
+let offset = { x: 0, y: 0 };
+
+//Start dragging
+function startDrag(event, minorGridSize, scale) {
+        
+    selectedElement = event.target.instance;
+
+    const point = selectedElement.point(event.clientX, event.clientY);
+    offset.x = point.x - selectedElement.x();
+    offset.y = point.y - selectedElement.y();
+
+    // Add event listeners for drag and end drag
+    //document.addEventListener('pointermove', drag);
+    document.addEventListener('pointermove', function(event) {    
+        drag(event, point, minorGridSize, scale);
+    });
+    
+
+    document.addEventListener('pointerup', endDrag);    
+  }
+
+//Actually drag the element
+function drag(event, oldPoint, minorGridSize, scale) {
+    if (selectedElement) {
+
+        let point = selectedElement.point(event.clientX, event.clientY) 
+        point.x = point.x - offset.x
+        point.y = point.y - offset.y
+        let roundedX = Math.round(point.x/(minorGridSize))*minorGridSize ;
+        let roundedY = Math.round(point.y/(minorGridSize))*minorGridSize ;   
+
+        //console.log("Moved to "+roundedX +" "+roundedY)
+        selectedElement.move(roundedX, roundedY);
+    }
+}
+
+
+// Function to end dragging
+function endDrag() {
+    
+    selectedElement = null;
+
+    // Remove event listeners for drag and end drag
+    document.removeEventListener('pointermove', drag);
+    document.removeEventListener('pointerup', endDrag);
+  }
+
+
+
+  
+///////////////////////////////
+//         Main Code!        //
+///////////////////////////////
 
 SVG.on(document, 'DOMContentLoaded', function() {
     
@@ -88,7 +151,7 @@ SVG.on(document, 'DOMContentLoaded', function() {
     
     //Calibration Figures to Mark Locations
     draw.rect(100,100).attr({fill: '#f00'}).move(0.25*width,0.25*height).draggable()
-    draw.rect(100,100).attr({fill: '#ff0'}).move(0.5*width,0.5*height).draggable()
+    draw.rect(100,100).attr({fill: '#ff0'}).move(0.5*width,0.5*height).draggable().onClick = function() { console.log("clicked!"); };
 
 
     //Change grid configuration by radio buttons
@@ -106,6 +169,8 @@ SVG.on(document, 'DOMContentLoaded', function() {
 
 
     const svgcontainer = document.getElementById('svg-container')
+    
+    
     const panzoom = Panzoom(svgcontainer, {
         maxScale: 5,
         minScale: 0.25,
@@ -154,24 +219,20 @@ SVG.on(document, 'DOMContentLoaded', function() {
 
     let roundedX = 0;
     let roundedY = 0;
-
-    let scaling = 0;    
     
 
     // Event listener to track mouse movement over the target element
     targetElement.addEventListener('mousemove', (event) => {
-        // Get the bounding rectangle of the element
-        const rect = targetElement.getBoundingClientRect();
         // Calculate mouse position relative to the element
-        mouseX = event.clientX - rect.left;
-        mouseY = event.clientY - rect.top;
-
-        scaling = panzoom.getScale();
-
-
-        roundedX = Math.round(mouseX/(minorGridSize*scaling))*minorGridSize ;
-        roundedY = Math.round(mouseY/(minorGridSize*scaling))*minorGridSize ;
+        selectedElement = event.target.instance;
+        let mousePoints = selectedElement.point(event.clientX, event.clientY);
+        
+        roundedX = Math.round(mousePoints.x/(minorGridSize))*minorGridSize ;
+        roundedY = Math.round(mousePoints.y/(minorGridSize))*minorGridSize ;
     });
+
+
+
 
     //ID counter for slat IDs
     let slatCounter = 0;
@@ -185,14 +246,34 @@ SVG.on(document, 'DOMContentLoaded', function() {
                 let tmpLine = draw.line(roundedX, roundedY, roundedX, roundedY + 32 * minorGridSize).stroke({ width: 3, color:'#076900' });
                 tmpLine.attr('id','ID-L'+'-N' + slatCounter)
                 tmpLine.attr('class',"line")
-                tmpLine.draggable();
+                tmpLine.attr({ 'pointer-events': 'stroke' })
                 slatCounter += 1;
+
+
+                //Experimental: dragging
+
+                // Attach event listener using SVG.js method
+                tmpLine.on('pointerdown', function(event) {
+                    // your parameters
+                    let scale = panzoom.getScale();
+                
+                    startDrag(event, minorGridSize, scale);
+                });
+                
+                tmpLine.on('pointerdown', startDrag);
+                
+                
 
             }
             
 
         }        
     });
+
+
+
+
+
 
 
 })
