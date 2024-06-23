bbd086a04cd37876341e03832e779f4b6a14b9a5
Corey Becker <cmbecker1000@gmail.com>
2024-06-23 16:21:49 -0400

    Added basic cargo
    Can place cargo on different layers. Overlap
    prevented.
    
    TODO:
    1. Make cargo movable
    2. Make cargo deletable
    3. Make cargo respond to color changes
    4. Make multiple types of cargo
        A. Maybe indicate different cargo via a number
           in the cargo circles? Might be a simple way
           to do this...

--- GUI/static/scripts/svgscript.js
+++ GUI/static/scripts/svgscript.js
@@ -16,7 +16,12 @@ let placeRoundedX = 0;                  //Snapped position of mouse (X)
 let placeRoundedY = 0;                  //Snapped posiiton of mouse (Y)
 
 //For drag & drop
-var activeLayer = null;
+var activeHandleLayer = null;
+var activeSlatLayer   = null;
+var activeCargoLayer  = null;
+
+//var activeLayer = null;
+
 var activeLayerId = null;
 var activeLayerColor = null;
 
@@ -32,10 +37,14 @@ let placeHorizontal = false;
 
 //ID counter for slat IDs
 let slatCounter = 0;
+let cargoCounter = 0;
 
 //Select
 let drawEraseSelectMode = 0; //0 for draw, 1 for erase, 2 for select
 let selectedColor = '#93f5f2';
+
+//Draw mode
+let drawSlatCargoHandleMode = 0; //0 for slats, 1 for cargo, 2 for handles
 
 
 
@@ -94,6 +103,10 @@ function drawGrid(gridGroup, width, height, style, majorSize, minorSize) {
     return gridGroup;
   }
 
+
+///////////////////////////////
+//  Slat Overlap Checkers    //
+///////////////////////////////
 
 // Check if a point is on any existing line
 function isPointOnLine(Layer, x, y, selectedLine = false) {
@@ -169,6 +182,96 @@ function willHorzBeOnLine(startX, startY, layer, gridSize, length=32){
 }
 
 
+
+///////////////////////////////
+//  Cargo Overlap Checkers   //
+///////////////////////////////
+
+function isCargoOnCargo(x, y, layer){
+    const cargos = layer.find('.cargo');
+    return cargos.some(cargo => {
+      
+        //Check if overlapping with any lines in general
+        const bbox = cargo.bbox();
+        let onOther = (x >= bbox.x && x <= bbox.x2 && y >= bbox.y && y <= bbox.y2)
+      
+        return (
+            onOther
+        );
+    });
+}
+
+
+
+
+///////////////////////////////
+//     Drawing Functions     //
+///////////////////////////////
+function placeSlat(roundedX, roundedY, activeSlatLayer, activeLayerId, minorGridSize, activeLayerColor, shownOpacity, slatCounter, horizontal) {
+    if(!horizontal){
+        if(!willVertBeOnLine(roundedX, roundedY, activeSlatLayer, minorGridSize, 32)) {
+            let defaultColor = activeLayerColor; 
+            let tmpLine = activeSlatLayer.line(roundedX, roundedY, roundedX, roundedY + 32 * minorGridSize)
+                                        .stroke({ width: 3, color:defaultColor, opacity: shownOpacity });
+            tmpLine.attr('id','ID-'+activeLayerId + '-N' + slatCounter)
+            tmpLine.attr('class',"line")
+            tmpLine.attr({ 'pointer-events': 'stroke' })
+            tmpLine.attr('data-default-color', defaultColor);
+
+            //Adding draggability:
+            tmpLine.on('pointerdown', startDrag)
+
+            slatCounter += 1;
+        }
+    }
+    else if(horizontal){
+        if(!willHorzBeOnLine(roundedX, roundedY, activeSlatLayer, minorGridSize, 32)) {
+            let defaultColor = activeLayerColor 
+            let tmpLine = activeSlatLayer.line(roundedX, roundedY, roundedX + 32 * minorGridSize, roundedY )
+                                        .stroke({ width: 3, color:defaultColor, opacity: shownOpacity });
+            tmpLine.attr('id','ID-'+activeLayerId+'-N' + slatCounter)
+            tmpLine.attr('class',"line")
+            tmpLine.attr({ 'pointer-events': 'stroke' })
+            tmpLine.attr('data-default-color', defaultColor);
+
+            //Adding draggability:
+            tmpLine.on('pointerdown', startDrag)
+
+            slatCounter += 1;
+        }
+    }
+
+    return slatCounter;
+}
+
+
+function placeCargo(roundedX, roundedY, activeCargoLayer, activeLayerId, minorGridSize, activeLayerColor, shownOpacity, cargoCounter) {
+        
+    let defaultColor = activeLayerColor; 
+
+    if(!isCargoOnCargo(roundedX, roundedY, activeCargoLayer)){
+        let tmpCircle = activeCargoLayer.circle(minorGridSize * 0.75) // SVG.js uses diameter, not radius
+                                        .attr({ cx: roundedX, cy: roundedY })
+                                        .fill(activeLayerColor) // You can set the fill color here
+                                        .opacity(shownOpacity);
+        tmpCircle.attr('id','CargoID-'+activeLayerId + '-N' + cargoCounter)
+        tmpCircle.attr('class',"cargo")
+        tmpCircle.attr('data-default-color', defaultColor)
+
+        //tmpLine.attr({ 'pointer-events': 'stroke' })
+
+        //Adding draggability:
+        tmpCircle.on('pointerdown', startDrag)
+
+        cargoCounter += 1;
+    }
+
+    
+    return cargoCounter;
+}
+
+
+
 ///////////////////////////////
 //       Drag and Drop       //
 ///////////////////////////////
@@ -179,7 +282,7 @@ function startDrag(event) {
         
     dragSelectedElement = event.target.instance;
 
-    if(activeLayer.children().includes(dragSelectedElement)){
+    if(activeSlatLayer.children().includes(dragSelectedElement)){
         
         //drawEraseSelectMode == 0 corresponds to drawing
         //drawEraseSelectMode == 1 corresponds to erasing
@@ -229,11 +332,7 @@ function drag(event) {
         let roundedX = Math.round(point.x/(minorGridSize))*minorGridSize ;
         let roundedY = Math.round(point.y/(minorGridSize))*minorGridSize ;   
 
-        //if(!isPointOnLine(activeLayer, roundedX, roundedY, dragSelectedElement)   && !isPointOnLine(activeLayer, roundedX, roundedY + 32 * minorGridSize, dragSelectedElement)){
-        //    dragSelectedElement.move(roundedX, roundedY);
-        //}
-
-        if(!isLineOnLine(roundedX, roundedY, activeLayer,minorGridSize, dragSelectedElement)) {
+        if(!isLineOnLine(roundedX, roundedY, activeSlatLayer,minorGridSize, dragSelectedElement)) {
             dragSelectedElement.move(roundedX, roundedY);
         }
 
@@ -267,15 +366,17 @@ function initializeSparseGridDictionary() {
     return {};
 }
 
+
 // Convert grid coordinates to a string key for the dictionary
 function gridKey(x, y, layer) {
     return `${x},${y},${layer}`;
 }
 
+
 // Populate the sparse grid dictionary with slat IDs
 function populateSparseGridDictionary(gridDict, layers) {
     layers.forEach((layer, layerIndex) => {
-        layer.children().forEach(child => {
+        layer[1].children().forEach(child => {
             let slatId = child.attr('id');
             let bbox = child.bbox();
             let startX = Math.round(bbox.x / minorGridSize);
@@ -295,7 +396,6 @@ function populateSparseGridDictionary(gridDict, layers) {
 }
 
 
-
 //Create array
 function createGridArray(layerList) {
     // Initialize the sparse grid dictionary
@@ -310,6 +410,12 @@ function createGridArray(layerList) {
     return gridDict
 }
   
+
+
+
+
+
+
 ///////////////////////////////
 //         Main Code!        //
 ///////////////////////////////
@@ -324,16 +430,10 @@ SVG.on(document, 'DOMContentLoaded', function() {
     
     //Layers
     var drawGridLayer = fullDrawing.group();
-    //var draw = fullDrawing.group();
-    //activeLayer = draw
+
 
     //Initialize Grid
     drawGrid(drawGridLayer, width, height, gridStyle, majorGridSize, minorGridSize)
-
-    
-    //Calibration Figures to Mark Locations
-    //draw.rect(100,100).attr({fill: '#f00'}).move(0.25*width,0.25*height).draggable()
-    //draw.rect(100,100).attr({fill: '#ff0'}).move(0.5*width,0.5*height).draggable().onClick = function() { console.log("clicked!"); };
 
 
     //Change grid configuration by radio buttons
@@ -440,45 +540,36 @@ SVG.on(document, 'DOMContentLoaded', function() {
         if(disablePanStatus == true){
             console.log(`Rounded mouse position - X: ${placeRoundedX}, Y: ${placeRoundedY}`);
 
-            if(!placeHorizontal){
-                if(!willVertBeOnLine(placeRoundedX, placeRoundedY, activeLayer, minorGridSize, 32)) {
-                    let defaultColor = activeLayerColor; //'#076900';
-                    let tmpLine = activeLayer.line(placeRoundedX, placeRoundedY, placeRoundedX, placeRoundedY + 32 * minorGridSize)
-                                                .stroke({ width: 3, color:defaultColor, opacity: shownOpacity });
-                    tmpLine.attr('id','ID-'+activeLayerId + '-N' + slatCounter)
-                    tmpLine.attr('class',"line")
-                    tmpLine.attr({ 'pointer-events': 'stroke' })
-                    tmpLine.attr('data-default-color', defaultColor);
-                    slatCounter += 1;
-    
-                    //Adding draggability:
-                    tmpLine.on('pointerdown', startDrag)
+            if(drawSlatCargoHandleMode == 0){
+                //Record old number of slats places
+                let oldSlatCounter = slatCounter;
 
-                    //Create grid array
+                //Place slat
+                slatCounter = placeSlat(placeRoundedX, placeRoundedY, activeSlatLayer, activeLayerId, minorGridSize, activeLayerColor, shownOpacity, slatCounter, placeHorizontal)
+
+                //Create grid array if a slat has been sucessfully placed!
+                if(oldSlatCounter < slatCounter){
                     createGridArray(layerList)
-                    }
+                }
             }
-            else if(placeHorizontal){
-                if(!willHorzBeOnLine(placeRoundedX, placeRoundedY, activeLayer, minorGridSize, 32)) {
-                    let defaultColor = activeLayerColor //'#836108';
-                    let tmpLine = activeLayer.line(placeRoundedX, placeRoundedY, placeRoundedX + 32 * minorGridSize, placeRoundedY )
-                                                .stroke({ width: 3, color:defaultColor, opacity: shownOpacity });
-                    tmpLine.attr('id','ID-'+activeLayerId+'-N' + slatCounter)
-                    tmpLine.attr('class',"line")
-                    tmpLine.attr({ 'pointer-events': 'stroke' })
-                    tmpLine.attr('data-default-color', defaultColor);
-                    slatCounter += 1;
-    
-                    //Adding draggability:
-                    tmpLine.on('pointerdown', startDrag)
+            else if(drawSlatCargoHandleMode == 1){
+                //Record old number of cargo places
+                let oldCargoCounter = cargoCounter;
 
-                    //Create grid array
-                    createGridArray(layerList)
-                    }
+                //Place cargo
+                cargoCounter = placeCargo(placeRoundedX, placeRoundedY, activeCargoLayer, activeLayerId, minorGridSize, activeLayerColor, shownOpacity, cargoCounter)
+                
+                //Create grid array if a cargo has been sucessfully placed!
+                if(oldCargoCounter < cargoCounter){
+                //    createGridArray(layerList)
+                }
             }
             
-            
 
+
+
+            
+            
         }        
     });
 
@@ -487,41 +578,61 @@ SVG.on(document, 'DOMContentLoaded', function() {
     document.addEventListener('layerAdded', (event) => {
         console.log(`Layer added: ${event.detail.layerId}`, event.detail.layerElement);
         //Layer added
-        layerList.set(event.detail.layerId, fullDrawing.group());
+        let handleGroup = fullDrawing.group();
+        let slatGroup = fullDrawing.group();
+        let cargoGroup = fullDrawing.group();
+        const tmpFullLayer = [handleGroup, slatGroup, cargoGroup];
+
+        layerList.set(event.detail.layerId, tmpFullLayer)
+
+        //layerList.set(event.detail.layerId, fullDrawing.group());
     });
 
     document.addEventListener('layerRemoved', (event) => {
         console.log(`Layer removed: ${event.detail.layerId}`, event.detail.layerElement);
         //Layer removed
-        layerList.get(event.detail.layerId).remove()
+        //layerList.get(event.detail.layerId).remove()
+        const fullLayer = layerList.get(event.detail.layerId);
+        fullLayer[0].remove();
+        fullLayer[1].remove();
+        fullLayer[2].remove();
         layerList.delete(event.detail.layerId)
     });
 
     document.addEventListener('layerShown', (event) => {
         console.log(`Layer shown: ${event.detail.layerId}`, event.detail.layerElement);
-        // Handle layer shown
-        layerList.get(event.detail.layerId).attr('opacity',1)
+        // Deal with layer shown
+        //layerList.get(event.detail.layerId).attr('opacity',1)
+        const fullLayer = layerList.get(event.detail.layerId)
+        fullLayer[0].attr('opacity',1)
+        fullLayer[1].attr('opacity',1)
+        fullLayer[2].attr('opacity',1)
     });
 
     document.addEventListener('layerHidden', (event) => {
         console.log(`Layer hidden: ${event.detail.layerId}`, event.detail.layerElement);
-        // Handle layer hidden
-        layerList.get(event.detail.layerId).attr('opacity',hiddenOpacity)
+        // Deal with layer hidden
+        //layerList.get(event.detail.layerId).attr('opacity',hiddenOpacity)
+        const fullLayer = layerList.get(event.detail.layerId)
+        fullLayer[0].attr('opacity', hiddenOpacity)
+        fullLayer[1].attr('opacity', hiddenOpacity)
+        fullLayer[2].attr('opacity', hiddenOpacity)
         
     });
 
     document.addEventListener('layerMarkedActive', (event) => {
         console.log(`Layer marked active: ${event.detail.layerId}`, event.detail.layerElement);
-        // Handle layer marked active
-        activeLayer = layerList.get(event.detail.layerId)
+        // Deal with layer marked active
+
+
         activeLayerId = event.detail.layerId
+
+        const fullLayer = layerList.get(event.detail.layerId)
+        activeHandleLayer = fullLayer[0]
+        activeSlatLayer   = fullLayer[1]
+        activeCargoLayer  = fullLayer[2]
+
         activeLayerColor = event.detail.layerColor
-        console.log(activeLayerId)
-
-
-
-        
-
 
     });
 
@@ -533,7 +644,9 @@ SVG.on(document, 'DOMContentLoaded', function() {
         console.log(`New color for ${layerId}: ${layerColor}`);
 
 
-        const layerToChange = layerList.get(event.detail.layerId)
+        //Only change slat layer colors
+        const fullLayer = layerList.get(event.detail.layerId)
+        const layerToChange = fullLayer[1]
 
         layerToChange.children().forEach(child => {
             child.stroke({ color: layerColor });
@@ -547,6 +660,19 @@ SVG.on(document, 'DOMContentLoaded', function() {
     
         // Your code to handle the color change, e.g., updating a UI element, applying the color to a canvas, etc.
     });
+
+
+    //Allow switching between draw modes
+        //0 for slats
+        //1 for cargo
+        //2 for handles
+    const drawModeSelector = document.getElementById('palette-type-selector');
+    drawModeSelector.addEventListener('change', function () {
+        drawSlatCargoHandleMode = drawModeSelector.value;
+        console.log("draw mode: "+drawSlatCargoHandleMode)
+    });
+    
+
         
 
 
