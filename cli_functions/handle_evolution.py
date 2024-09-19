72c378e997c1f787b14b5b8b9b5d8af462556bfe
matt <matthewaq31@gmail.com>
2024-09-19 16:46:27 -0400

    quick patch to allow full designs to be used for hamming evolution

--- cli_functions/handle_evolution.py
+++ cli_functions/handle_evolution.py
@@ -16,8 +16,16 @@ def handle_evolve(config_file):
 
     # reads in design here
     design_df = pd.read_excel(evolution_params['slat_array'], sheet_name=None, header=None)
-    slat_array = np.zeros((design_df['slat_layer_1'].shape[0], design_df['slat_layer_1'].shape[1], len(design_df)))
+
+    total_layers = 0
     for i, key in enumerate(design_df.keys()):
+        if 'slat_layer_' in key:
+            total_layers += 1
+        
+    slat_array = np.zeros((design_df['slat_layer_1'].shape[0], design_df['slat_layer_1'].shape[1], total_layers))
+    for i, key in enumerate(design_df.keys()):
+        if 'slat_layer_' not in key:
+            continue
         slat_array[..., i] = design_df[key].values
 
     slat_array[slat_array == -1] = 0 # knocks out any seed positions
