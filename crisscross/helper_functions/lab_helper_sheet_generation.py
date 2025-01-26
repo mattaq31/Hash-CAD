added default_reaction_volume option to lab helper sheets for more efficient use of pooled slats; also a quick fix to the molecular weight calculation for PEG, previously didn't account for loss dehydration between bases and overestimated weight

crisscross/helper_functions/lab_helper_sheet_generation.py
@@ -4,6 +4,7 @@ from openpyxl import Workbook
 from openpyxl.formatting.rule import CellIsRule
 from openpyxl.styles import PatternFill, Font, Border, Side, Alignment
 import string
+import math
 
 
 uppercase_alphabet = string.ascii_uppercase
@@ -57,7 +58,7 @@ def adjust_column_width(ws):
 
 
 def prepare_master_mix_sheet(slat_dict, echo_sheet=None, default_staple_volume=150, default_staple_concentration=500,
-                             unique_transfer_volume_plates=None, workbook=None):
+                             default_reaction_volume=50, unique_transfer_volume_plates=None, workbook=None):
 
     slat_count = len(slat_dict)
     min_slat_concentration = np.inf
@@ -127,7 +128,12 @@ def prepare_master_mix_sheet(slat_dict, echo_sheet=None, default_staple_volume=1
     ws['D5'] = "=round(C5*D$8/B5,2)"
     ws['D6'] = "=round(C6*D$8/B6,2)"
     ws['D7'] = '=D8-sum(D2:D6)'
-    ws['D8'] = 50
+    if default_reaction_volume == "max":
+        # selects the max reaction volume that reduces staple pool waste, assuming echo transfers only 75% of expected
+        # 0.75 * total femtomoles of slats (based on min conc)/reaction final conc (500 nM), rounded down to the nearest multiple of 5
+        ws['D8'] = math.floor(0.75 * min_slat_concentration * total_handle_mix_volume/ws['C2'].value/5/1000) * 5
+    else:
+        ws['D8'] = default_reaction_volume # simplest solution: user decides, defaults to 50 µL
     ws['D9'] = '=D8*C3/1000'
 
     ws['F2'].fill = red_fill
@@ -185,8 +191,8 @@ def prepare_master_mix_sheet(slat_dict, echo_sheet=None, default_staple_volume=1
 
     return wb
 
-def prepare_peg_purification_sheet(slat_dict, groups_per_layer=2, max_slat_concentration_uM=2, workbook=None,
-                                   echo_sheet=None, special_slat_groups=None):
+def prepare_peg_purification_sheet(slat_dict, groups_per_layer=2, max_slat_concentration_uM=2, default_reaction_volume=50, 
+                                   workbook=None, echo_sheet=None, special_slat_groups=None):
     if workbook is not None:
         wb = workbook
     else:
@@ -270,7 +276,11 @@ def prepare_peg_purification_sheet(slat_dict, groups_per_layer=2, max_slat_conce
         column = uppercase_alphabet[position+1]
         # block 1 - slat counts and volumes
         full_data_groups[group][f'{column}1'] = group
-        full_data_groups[group][f'{column}3'] = 50
+        try:
+            full_data_groups[group][f'{column}3'] = wb["Slat Folding & Master Mix"]["D8"].value
+        except KeyError:
+            print("No Slat Folding & Master Mix sheet detected. Defaulting to 50 µL reaction volume...")
+            full_data_groups[group][f'{column}3'] = 50
         full_data_groups[group][f'{column}4'] = len(full_data_groups[group]['IDs'])
         full_data_groups[group][f'{column}5'] = f"={column}3*{column}4"
         full_data_groups[group][f'{column}6'] = 50
@@ -389,6 +399,7 @@ def prepare_peg_purification_sheet(slat_dict, groups_per_layer=2, max_slat_conce
 
 def prepare_all_standard_sheets(slat_dict, save_filepath, default_staple_volume=150,
                                 default_staple_concentration=500,
+                                default_reaction_volume=50,
                                 peg_groups_per_layer=2,
                                 echo_sheet=None,
                                 max_slat_concentration_uM=2,
@@ -400,6 +411,7 @@ def prepare_all_standard_sheets(slat_dict, save_filepath, default_staple_volume=
     :param save_filepath:
     :param default_staple_volume:
     :param default_staple_concentration:
+    :param default_reaction_volume:
     :param peg_groups_per_layer:
     :param echo_sheet:
     :param max_slat_concentration_uM:
@@ -411,7 +423,7 @@ def prepare_all_standard_sheets(slat_dict, save_filepath, default_staple_volume=
     wb.remove(wb["Sheet"])
 
     prepare_master_mix_sheet(slat_dict, echo_sheet, default_staple_volume, default_staple_concentration,
-                             unique_transfer_volume_plates, wb)
-    prepare_peg_purification_sheet(slat_dict, peg_groups_per_layer, max_slat_concentration_uM, wb,
-                                   echo_sheet=echo_sheet, special_slat_groups=special_slat_groups)
+                             default_reaction_volume, unique_transfer_volume_plates, wb)
+    prepare_peg_purification_sheet(slat_dict, peg_groups_per_layer, max_slat_concentration_uM, default_reaction_volume, 
+                                   wb, echo_sheet=echo_sheet, special_slat_groups=special_slat_groups)
     wb.save(save_filepath)

scripts/SW119_newlibkinetics/SW119_echoprotocol.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import os
 import numpy as np
+import matplotlib.pyplot as plt
 
 from crisscross.core_functions.megastructures import Megastructure
 from crisscross.core_functions.megastructure_composition import convert_slats_into_echo_commands
 from crisscross.assembly_handle_optimization.hamming_compute import multirule_precise_hamming
-from crisscross.plate_mapping import get_standard_plates, get_cargo_plates
+from crisscross.plate_mapping import get_standard_plates
+from crisscross.helper_functions.lab_helper_sheet_generation import prepare_all_standard_sheets
 
 DesignFolder = "/Users/stellawang/HMS Dropbox/Siyuan Wang/crisscross_team/Crisscross Designs/SW119_newlibkinetics"
 LayerListPrefix = "slat_layer_" 
@@ -13,24 +15,25 @@ SeedLayer = "seed_layer"
 HandleArrayPrefix = "handle_layer_"
 DesignFile = "/Users/stellawang/HMS Dropbox/Siyuan Wang/crisscross_team/Crisscross Designs/SW119_newlibkinetics/newlibrary_zigzagribbon_design.xlsx"
 
-RunAnalysis = True
+RunAnalysis = False
 
 CorePlate, CrisscrossAntihandleYPlates, CrisscrossHandleXPlates, EdgeSeedPlate, CenterSeedPlate, CombinedSeedPlate = get_standard_plates()
 
 # Specify wells for more intuitive echo protocol
-EightByEightWells = [[(1,x+str(y)) for y in np.arange(1,8+1,1)] for x in ["A","B","C","D","E","F","G","H"]]
+EightByEightWells = [[(1,x+str(y)) for y in np.arange(3,10+1,1)] for x in ["A","B","C","D","E","F","G","H"]]
 EightByEightWellsFlat = [item for sublist in EightByEightWells for item in sublist]
 
 # In terms of Hamming calculation, we are testing possible interactions between slats (self-substition) and whether the next growth attaches correctly to the exposed region
 
-SlatGrouping = {"Right":[(1, x) for x in np.arange(1,16+1,1)], "Up":[(2, x) for x in np.arange(17,32+1,1)]}
+SlatGrouping = {"Right-Up":[(1, x) for x in np.arange(1,16+1,1)] + [(2,y) for y in np.arange(17,32+1,1)]}
 
 PartialAreaGrouping = {"Right1-Up1":{}, "Right1-Up2":{}}
 
 # Right growth slats against antihandle set for Block 1
 PartialAreaGrouping["Right1-Up1"]["handles"] = {}
 for slatID in np.arange(1,16+1,1):
-    PartialAreaGrouping["Right1-Up1"]["handles"][(1,slatID)] = [False for _ in np.arange(0,16,1)] + [True for _ in np.arange(0,16,1)]
+    #PartialAreaGrouping["Right1-Up1"]["handles"][(1,slatID)] = [False for _ in np.arange(0,16,1)] + [True for _ in np.arange(0,16,1)]
+    PartialAreaGrouping["Right1-Up1"]["handles"][(1,slatID)] = [True for _ in np.arange(0,32,1)]
 
 PartialAreaGrouping["Right1-Up1"]["antihandles"] = {}
 for slatID in np.arange(17,32+1,1):
@@ -39,7 +42,8 @@ for slatID in np.arange(17,32+1,1):
 # Right growth slats against antihandle set for Block 2
 PartialAreaGrouping["Right1-Up2"]["handles"] = {}
 for slatID in np.arange(1,16+1,1):
-    PartialAreaGrouping["Right1-Up2"]["handles"][(1,slatID)] = [False for _ in np.arange(0,16,1)] + [True for _ in np.arange(0,16,1)]
+    #PartialAreaGrouping["Right1-Up2"]["handles"][(1,slatID)] = [False for _ in np.arange(0,16,1)] + [True for _ in np.arange(0,16,1)]
+    PartialAreaGrouping["Right1-Up2"]["handles"][(1,slatID)] = [True for _ in np.arange(0,32,1)]
 
 PartialAreaGrouping["Right1-Up2"]["antihandles"] = {}
 for slatID in np.arange(17,32+1,1):
@@ -72,6 +76,9 @@ HandleArray = np.zeros((DesignDF[HandleLayers[0]].shape[0], DesignDF[HandleLayer
 for i, key in enumerate(HandleLayers):
     HandleArray[..., i] = DesignDF[key].values
 
+# Save the seed array
+SeedArray = DesignDF[SeedLayer].values
+
 # Don't do Hamming distance calculation for the "add-ons", just the square "alphabet" design
 if RunAnalysis:
     result = multirule_precise_hamming(SlatArray, HandleArray, per_layer_check=True, specific_slat_groups=SlatGrouping, request_substitute_risk_score=True)
@@ -79,10 +86,7 @@ if RunAnalysis:
     # First report Hamming distance measures if two sets of slat blocks are exposed to each other
     print('Hamming distance (global): %s' % result['Universal']) 
     print('Hamming distance (substitution risk): %s' % result['Substitute Risk'])
-    print('Hamming distance (groups Right & Up): %s' % result['Right-Up'])
-    print('Hamming distance (groups Left & Up): %s' % result['Left-Up'])
-    print('Hamming distance (groups Right & Down): %s' % result['Right-Down'])
-    print('Hamming distance (groups Left & Down): %s' % result['Left-Down'])
+    print('Hamming distance (groups Right): %s' % result['Right-Up'])
     
     result_partial = multirule_precise_hamming(SlatArray, HandleArray, per_layer_check=True, request_substitute_risk_score=True, \
                                             partial_area_score=PartialAreaGrouping)
@@ -90,8 +94,49 @@ if RunAnalysis:
     for subgroup in ["Right1-Up1", "Right1-Up2"]:
         print('Hamming distance ({} handle-antihandles): {}'.format(subgroup, result_partial[subgroup]))
 
+# Handle the megastructure creation here
+OldLibraryMega = Megastructure(SlatArray, [2, (5, 2), 5])
+OldLibraryMega.assign_crisscross_handles(HandleArray, CrisscrossHandleXPlates, CrisscrossAntihandleYPlates)
+OldLibraryMega.assign_seed_handles(SeedArray, CombinedSeedPlate, layer_id=1)
+
+# Convert labels and handles into actual wells from source plates (patching)
+#OldLibraryMega.patch_placeholder_handles([CrisscrossHandleXPlates, CrisscrossAntihandleYPlates, CombinedSeedPlate], \
+#                                            ['Assembly-Handles', 'Assembly-AntiHandles', 'Seed'])
+OldLibraryMega.patch_control_handles(CorePlate)
+
+# Generate csv file for each set of sequences, combine them later (add volume to the echo command generating funtion)
+# Filter out only the slats we need
+
+convert_slats_into_echo_commands(OldLibraryMega.slats, "old_vs_new_library_kinetics", 
+                                 DesignFolder, "SW119_zigzag_oldlibrary_echo.csv",
+                                 default_transfer_volume=200,
+                                 manual_plate_well_assignments=EightByEightWellsFlat[:32])
+
+print("Finished making echo protocol for %s." % DesignFile)
+
+
 # Make the exact same slats but for the new library: Note, may need to watch for wells with higher or lower volumes/concentrations 
 # - should be accounted for in the code
+
+# Get new assembly handle plates
+_, v2_crisscross_antihandle_y_plates, v2_crisscross_handle_x_plates, _, _, _ = get_standard_plates(handle_library_v2=True)
+
+NewLibraryMega = Megastructure(SlatArray, [2, (5, 2), 5])
+NewLibraryMega.assign_crisscross_handles(HandleArray, v2_crisscross_handle_x_plates, v2_crisscross_antihandle_y_plates)
+NewLibraryMega.assign_seed_handles(SeedArray, CombinedSeedPlate, layer_id=1)
+NewLibraryMega.patch_control_handles(CorePlate)
+
+EchoSheetNew = convert_slats_into_echo_commands(NewLibraryMega.slats, "old_vs_new_library_kinetics", 
+                                 DesignFolder, "SW119_zigzag_newlibrary_echo.csv",
+                                 default_transfer_volume=200,
+                                 manual_plate_well_assignments=EightByEightWellsFlat[-32:])
+
+prepare_all_standard_sheets(NewLibraryMega.slats, os.path.join(DesignFolder, 'newlibrary_standard_helpers.xlsx'),
+                                default_staple_volume=200,
+                                default_staple_concentration=500,
+                                default_reaction_volume="max",
+                                echo_sheet=EchoSheetNew,
+                                peg_groups_per_layer=1)
 
 
 