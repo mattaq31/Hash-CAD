a7ad44c308222e85357a9984141bee00589ffdaa
yichenzhao <xiaozhao81@gmail.com>
2025-02-06 10:07:40 -0500

    changed vol to 100

--- scripts/glass conjugation/design2.py
+++ scripts/glass conjugation/design2.py
@@ -9,6 +9,7 @@ from crisscross.helper_functions import create_dir_if_empty
 from crisscross.helper_functions.lab_helper_sheet_generation import prepare_all_standard_sheets
 from crisscross.helper_functions.plate_constants import seed_plug_plate_center_8064, core_plate_folder
 from crisscross.plate_mapping import get_standard_plates, get_cargo_plates, get_plateclass
+from scripts.antigen_presenting_cells.capc_pattern_generator import capc_pattern_generator
 
 design_folder = '/Users/yichenzhao/Documents/Wyss/Projects/CrissCross_Output/FinalCross'
 echo_folder = os.path.join(design_folder, 'echo_commands')
@@ -30,10 +31,24 @@ generate_graphical_report = True
 M2 = Megastructure(import_design_file=os.path.join(design_folder, 'full_design_final_with_optimized_hamming.xlsx'))
 
 M2.slats[f'layer1-slat128'].reverse_direction()
+M2.slats[f'layer1-slat126'].reverse_direction()
+M2.slats[f'layer1-slat124'].reverse_direction()
+M2.slats[f'layer1-slat122'].reverse_direction()
+M2.slats[f'layer1-slat120'].reverse_direction()
+M2.slats[f'layer1-slat118'].reverse_direction()
+M2.slats[f'layer1-slat116'].reverse_direction()
 
 final_cargo_placement = np.zeros_like(M2.handle_arrays).squeeze()
 final_cargo_placement[47, 0] = 1
+final_cargo_placement[45, 0] = 1
+final_cargo_placement[43, 0] = 1
+final_cargo_placement[41, 0] = 1
+final_cargo_placement[39, 0] = 1
+final_cargo_placement[37, 0] = 1
+final_cargo_placement[35, 0] = 1
 
+cargo_array_pd = capc_pattern_generator('peripheral_dispersed', total_cd3_antigens=192, capc_length=66)
+M2.assign_cargo_handles_with_array(cargo_array_pd, cargo_key={1: 'antiBart', 2: 'antiEdna'}, layer='top')
 M2.assign_cargo_handles_with_array(final_cargo_placement, cargo_key={1: 'SSW041DoublePurification5primetoehold'}, layer='bottom')
 
 M2.patch_placeholder_handles(
@@ -53,11 +68,11 @@ if generate_graphical_report:
     M2.create_standard_graphical_report(os.path.join(design_folder, 'visualization/'),
                                         colormap='Set1',
                                         cargo_colormap='Dark2',
-                                        generate_3d_video=False,
+                                        generate_3d_video=True,
                                         seed_color=(1.0, 1.0, 0.0))
 
 if generate_echo:
-    target_volume = 130
+    target_volume = 100
     special_vol_plates = {'sw_src007': int(target_volume * (500 / 200)),
                           'sw_src004': int(target_volume * (500 / 200)),
                           'P3621_SSW': int(target_volume * (500 / 200)),
@@ -80,7 +95,7 @@ if generate_echo:
 
 if generate_lab_helpers:
     prepare_all_standard_sheets(M2.slats, os.path.join(lab_helper_folder, f'{experiment_name}_standard_helpers.xlsx'),
-                                default_staple_volume=150,
+                                default_staple_volume=target_volume,
                                 default_staple_concentration=500,
                                 echo_sheet=None if not generate_echo else echo_sheet,
                                 peg_groups_per_layer=4,
