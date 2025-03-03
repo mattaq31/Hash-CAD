aa6c3afbc462dba9f9c9163df468e1598bef8738
matt <matthewaq31@gmail.com>
2025-03-03 15:05:38 -0500

    Minor updates to evolution script

--- scripts/3D_stacking/evolution_version_2.py
+++ scripts/3D_stacking/evolution_version_2.py
@@ -21,7 +21,7 @@ slurm_parameters = {
 evolution_parameters = {
     'early_hamming_stop': 31,
     'evolution_generations': 20000,
-    'evolution_population': 300,
+    'evolution_population': 800,
     'process_count': 18,
     'generational_survivors': 5,
     'mutation_rate': 0.03,
@@ -39,24 +39,25 @@ all_sbatch_commands = []
 
 
 for seq_count in [32, 64]:
-    for mut_rate in [0.03, 0.2]:
-        exp_name = f'version_2_{seq_count}_seq_library_mut_{mut_rate}'
+    for mut_rate in [0.014, 0.03]:
+        exp_name = f'version_2_{seq_count}_seq_library_mut_{str(mut_rate).replace(".","-")}'
 
         server_experiment_folder = os.path.join(server_base_folder, exp_name)
         server_design_folder = os.path.join(server_base_folder)
         server_toml_file = os.path.join(server_experiment_folder, 'evolution_config.toml')
-        output_folder = batch_file_folder
+        output_folder = os.path.join(batch_file_folder, exp_name)
+        create_dir_if_empty(output_folder)
 
         evolution_parameters['log_tracking_directory'] = server_experiment_folder
         evolution_parameters['slat_array'] = os.path.join(server_design_folder, design_1)
         evolution_parameters['unique_handle_sequences'] = seq_count
         evolution_parameters['mutation_rate'] = mut_rate
 
-        with open(os.path.join(output_folder, f'evolution_config_{seq_count}_handles_mut_{mut_rate}.toml'), "w") as f:
+        with open(os.path.join(output_folder, f'evolution_config.toml'), "w") as f:
             toml.dump(evolution_parameters, f)
 
         slurm_batch = create_o2_slurm_file(**slurm_parameters, command=f'handle_evolve -c {server_toml_file}')
-        slurm_file  = os.path.join(output_folder, f'server_call_{seq_count}_handles_mut_{mut_rate}.sh')
+        slurm_file  = os.path.join(output_folder, f'server_call.sh')
         server_slurm_file = os.path.join(server_experiment_folder, 'server_call.sh')
 
         with open(slurm_file, 'w') as f:  # writes batch file out for use
