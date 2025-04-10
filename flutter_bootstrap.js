7a9ae918e5db30d994135ae13d2c1723b0247b99
mattaq31 <36590050+mattaq31@users.noreply.github.com>
2025-04-10 14:25:19 +0000

    Deploying to gh-pages from @ mattaq31/Crisscross-Design@9cf418aee63b980576e480d2b9ab8989ba363522 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1864517143"
+    serviceWorkerVersion: "815947183"
   }
 });
