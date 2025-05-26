38feb8813d991788db37a57d2399c84b3c67a52e
matt <matthewaq31@gmail.com>
2025-05-26 17:19:04 -0400

    version bump

--- flutter_app/lib/app_management/version_tracker.dart
+++ flutter_app/lib/app_management/version_tracker.dart
@@ -1,6 +1,6 @@
 class VersionInfo {
-  static const String version = 'v0.0.4';  // Will be replaced during build process
-  static const String buildNumber = '404';  // Will be replaced during build process
-  static const String buildCommit = 'a64ee35';  // Will be replaced during build process
-  static const String buildDate = '2025-05-26T15:05:48.507980';  // Will be replaced during build process
+  static const String version = 'v0.0.5';  // Will be replaced during build process
+  static const String buildNumber = '405';  // Will be replaced during build process
+  static const String buildCommit = 'dfd98d2';  // Will be replaced during build process
+  static const String buildDate = '2025-05-26T17:13:32.811141';  // Will be replaced during build process
 }
