aa6cf216e40962a19c2c64022c15b1a4125e3893
matt <matthewaq31@gmail.com>
2025-05-31 19:30:36 -0400

    Reverted github actions workflow - mac still not working

--- flutter_app/lib/app_management/version_tracker.dart
+++ flutter_app/lib/app_management/version_tracker.dart
@@ -1,6 +1,6 @@
 class VersionInfo {
   static const String version = 'v0.0.7';  // Will be replaced during build process
-  static const String buildNumber = '412';  // Will be replaced during build process
-  static const String buildCommit = '10aedc7';  // Will be replaced during build process
-  static const String buildDate = '2025-05-29T23:37:23.868496';  // Will be replaced during build process
+  static const String buildNumber = '417';  // Will be replaced during build process
+  static const String buildCommit = '4a24cdc';  // Will be replaced during build process
+  static const String buildDate = '2025-05-31T18:24:18.213501';  // Will be replaced during build process
 }
