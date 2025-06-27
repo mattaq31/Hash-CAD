07e2742e805431cc4d0a477731e9ba4c673e257b
matt <matthewaq31@gmail.com>
2025-06-27 19:54:39 -0400

    assembly handle view bugfix

--- flutter_app/lib/graphics/3d_painter.dart
+++ flutter_app/lib/graphics/3d_painter.dart
@@ -566,7 +566,7 @@ class _ThreeDisplay extends State<ThreeDisplay> {
       }
     }
 
-    if (existingHandle && (assemblyHandleView && handleType == 'Assembly' || cargoHandleView && handleType == 'CARGO' || seedHandleView && handleType == 'SEED')) {
+    if (existingHandle && (assemblyHandleView && handleType.contains('ASSEMBLY') || cargoHandleView && handleType == 'CARGO' || seedHandleView && handleType == 'SEED')) {
       Color color = handleType.contains('ASSEMBLY') ? layerMap[slat.layer]!['color']: handleType == 'CARGO' ? cargoPalette[cargoName]!.color: cargoPalette['SEED']!.color;
       positionHandleInstance(slat.id, handleName, position, color, order, topSide, handleSide, handleType, handleInstanceExists);
     } else if (handleInstanceExists){
