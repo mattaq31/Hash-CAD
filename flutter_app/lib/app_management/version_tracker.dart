6352838f21286e00bbe3cc282a932585a835e89c
matt <matthewaq31@gmail.com>
2026-01-26 11:28:30 -0500

    updated version number

--- flutter_app/lib/app_management/version_tracker.dart
+++ flutter_app/lib/app_management/version_tracker.dart
@@ -1,6 +1,6 @@
 class VersionInfo {
-  static const String version = 'v0.4.5';  // Will be replaced during build process
-  static const String buildNumber = '730';  // Will be replaced during build process
-  static const String buildCommit = 'c1c27d3';  // Will be replaced during build process
-  static const String buildDate = '2026-01-11T17:17:45.647173';  // Will be replaced during build process
+  static const String version = 'v1.0.0';  // Will be replaced during build process
+  static const String buildNumber = '771';  // Will be replaced during build process
+  static const String buildCommit = 'b24b6220';  // Will be replaced during build process
+  static const String buildDate = '2026-01-26T11:28:03.122194';  // Will be replaced during build process
 }
